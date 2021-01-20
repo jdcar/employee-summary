c31ea82c0b0091acb91aaea35e81f644aab23f82
jdcar <jdcarlstone@gmail.com>
2021-01-20 14:02:31 -0600

    Intern tests pass

--- test/Manager.test.js
+++ test/Manager.test.js
@@ -18,5 +18,6 @@ test("Can get office number via getOffice()", () => {
   const e = new Manager("Foo", 1, "test@test.com", testValue);
   expect(e.getOffice()).toBe
   // expect(e.getOfficeNumber()).toBe
+  // Changed getOfficeNumber to getOffice
   (testValue);
 });
